6ce675ba40da88cbb49767fcb325442621ba12ee
catarinasapateiro <catarinasapateiro@hotmail.com>
2025-04-29 16:31:33 +0100

    7-patch_/api/articles/id

--- app/models/model.js
+++ app/models/model.js
@@ -94,10 +94,30 @@ const insertComment = (username, body, article_id) => {
   }
 };
 
+const updateArticle = (newVote, article_id) => {
+  return db
+    .query(
+      `UPDATE articles SET votes = votes + $1
+    WHERE article_id = $2 RETURNING*;`,
+      [newVote, article_id]
+    )
+    .then((result) => {
+      if (result.rows.length === 0) {
+        return Promise.reject({
+          status: 404,
+          msg: "Article id not found",
+        });
+      } else {
+        return result.rows[0];
+      }
+    });
+};
+
 module.exports = {
   selectTopics,
   selectArticlesById,
   selectArticles,
   selectCommentsByArticleId,
   insertComment,
+  updateArticle,
 };
